de29eaa33695a8a434af33e8116677d7bf9e0153
Alberto <a.gonzalez7@sapalomera.cat>
2024-04-17 10:46:07 +0200

    Classe Electronica afegida.

---
+++ src/Electronica.java
@@ -0,0 +1,21 @@
+public class Electronica extends Producte{
+
+    int dies_garantia;
+
+    public Electronica(String nom, float preu, int codi_barres, int dies_garantia) {
+        super(nom, preu, codi_barres);
+        this.dies_garantia = dies_garantia;
+    }
+
+    @Override
+    public double calcularPreuTotal() {
+        //Calculem el preu segons els dies que hi ha de garantia.
+        return getPreu() + getPreu() * (dies_garantia / 365.0) * 0.1;
+    }
+
+    // Falta modificar conforme enunciat. Simple per comprovar.
+    @Override
+    public String toString() {
+        return super.toString() + " - Dies garantia: " + dies_garantia;
+    }
+}
