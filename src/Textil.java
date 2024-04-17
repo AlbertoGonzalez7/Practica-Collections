4243e1d217cba413bdb4dc24e4101291b296c98c
Alberto <a.gonzalez7@sapalomera.cat>
2024-04-17 10:58:56 +0200

    Classe Textil afegida.

---
+++ src/Textil.java
@@ -0,0 +1,20 @@
+public class Textil extends Producte{
+
+    String composicio_textil;
+
+    public Textil(String nom, float preu, int codi_barres, String composicio_textil) {
+        super(nom, preu, codi_barres);
+        this.composicio_textil = composicio_textil;
+    }
+
+    @Override
+    public double calcularPreuTotal() {
+        return getPreu();
+    }
+
+    // Falta modificar conforme enunciat. Simple per comprovar.
+    @Override
+    public String toString() {
+        return super.toString() + " - Composició Tèxtil: " + composicio_textil;
+    }
+}
